95f02df59df63ce38e553490ff9cc6019b74c153
DESKTOP-GU5HQF0\departamento <jmpadilla@ceti.mx>
2018-07-26 14:11:16 -0500

    add files

---
+++ materialize/script.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Botón',
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
